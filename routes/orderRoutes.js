1c88711bed9ee4456427bc057fd112c1fea92032
mrhackerhacker696-svg <mrhackerhacker696@gmail.com>
2026-05-03 22:51:24 +0530

    dsd

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -80,7 +80,7 @@ router.post('/', protect, async (req, res) => {
 // Admin: Get all orders
 router.get('/', protect, adminOnly, async (req, res) => {
   try {
-    const orders = await Order.find().sort({ createdAt: -1 });
+    const orders = await Order.find().populate('products.productId').sort({ createdAt: -1 });
     res.json(orders);
   } catch (err) {
     res.status(500).json({ message: err.message });
